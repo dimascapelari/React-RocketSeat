e5f5c4641d25f6f7a33e474230baa7c7772c3a07
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-24 12:26:37 -0300

    React RocketSeat

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
