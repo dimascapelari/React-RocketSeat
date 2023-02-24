42c90cc4cac442761ba1e40f23f2fc9bc35cba7e
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-24 14:42:30 -0300

    React RocketSeat

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
 };
