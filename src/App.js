b343c6f02570070ea57b8a7b98106f1d71c59642
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-24 14:53:51 -0300

    Importando Foto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import fotoDimas from "./assets/Dimas.jpg";
+
 function App() {
-  return <h1>Hello RocketSeat</h1>;
+  return (
+    <div>
+      <h1>Hello RocketSeat</h1>
+      <img src={fotoDimas} width="200" />
+    </div>
+  );
 }
 
 export default App;
